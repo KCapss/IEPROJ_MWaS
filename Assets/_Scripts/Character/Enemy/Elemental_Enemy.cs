c0002fab84d8ebe019e65bbe5e03a94070a8d8a6
Jose Romulo Guzman <84124290+DaUndeadGuy@users.noreply.github.com>
2023-12-01 22:04:46 +0800

    Testing Shaders

--- Assets/_Scripts/Character/Enemy/Elemental_Enemy.cs
+++ Assets/_Scripts/Character/Enemy/Elemental_Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class Elemental_Enemy : Enemy
@@ -23,20 +24,21 @@ public class Elemental_Enemy : Enemy
         int randomValue = Random.Range(0, 2);
 
         SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        Material material= spriteRenderer.material;
 
         if (randomValue == 0)
         {
-            spriteRenderer.color = Color.red;
+            material.SetColor("_SineGlowColor", UnityEngine.Color.red);
             currentWeakness = DamageType.Fire;
         }
         else if (randomValue == 1)
         {
-            spriteRenderer.color = Color.blue;
+            material.SetColor("_SineGlowColor", UnityEngine.Color.blue);
             currentWeakness = DamageType.Water;
         }
         else
         {
-            spriteRenderer.color = Color.green;
+            material.SetColor("_SineGlowColor", UnityEngine.Color.green);
             currentWeakness = DamageType.Wind;
         }
     }
@@ -210,18 +212,18 @@ public class Elemental_Enemy : Enemy
         int randomValue = Random.Range(0, 2);
 
         SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-
+        Material material = spriteRenderer.material;
         if(randomValue == 0)
         {
             if(currentWeakness == DamageType.Fire)
             {
                 currentWeakness = DamageType.Water;
-                spriteRenderer.color = Color.blue;
+                material.SetColor("_SineGlowColor", UnityEngine.Color.blue);
             }
             else
             {
                 currentWeakness = DamageType.Fire;
-                spriteRenderer.color = Color.red;
+                material.SetColor("_SineGlowColor", UnityEngine.Color.red);
             }
         }
         else if(randomValue == 1)
@@ -229,12 +231,12 @@ public class Elemental_Enemy : Enemy
             if(currentWeakness == DamageType.Water)
             {
                 currentWeakness = DamageType.Wind;
-                spriteRenderer.color = Color.green;
+                material.SetColor("_SineGlowColor", UnityEngine.Color.green);
             }
             else
             {
                 currentWeakness = DamageType.Water;
-                spriteRenderer.color = Color.blue;
+                material.SetColor("_SineGlowColor", UnityEngine.Color.blue);
             }
         }
 
@@ -243,12 +245,12 @@ public class Elemental_Enemy : Enemy
             if(currentWeakness == DamageType.Wind)
             {
                 currentWeakness = DamageType.Fire;
-                spriteRenderer.color = Color.red;
+                material.SetColor("_SineGlowColor", UnityEngine.Color.red);
             }
             else
             {
                 currentWeakness = DamageType.Wind;
-                spriteRenderer.color = Color.green;
+                material.SetColor("_SineGlowColor", UnityEngine.Color.green);
             }
         }
 
