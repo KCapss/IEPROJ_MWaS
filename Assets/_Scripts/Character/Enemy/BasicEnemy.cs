9bdb907280ab7d09f20efc27a344dfd8d802f283
SamohtB <75974322+SamohtB@users.noreply.github.com>
2023-12-02 22:55:06 +0800

    Balance Changes
    -Reduced cooldowns

--- Assets/_Scripts/Character/Enemy/BasicEnemy.cs
+++ Assets/_Scripts/Character/Enemy/BasicEnemy.cs
@@ -67,7 +67,6 @@ public class BasicEnemy : Enemy
             float fillAmount = 1.0f - timer;
             material.SetFloat("_FullDistortionFade", fillAmount);
 
-
             timer += Time.deltaTime;
             Debug.Log(fillAmount);
             yield return null;
